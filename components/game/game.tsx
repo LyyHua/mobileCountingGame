8f57f1cf037a89254bcfd88c77617296478c947a
LyyHua <lyhuavanly@gmail.com>
2024-08-01 22:19:29 +0700

    So far i already did all of the necessary component now i just need to do ui which i fucking hate tho

--- components/game/game.tsx
+++ components/game/game.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import { Alert, StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { ThemedText } from "../ThemedText";
 import { ThemedView } from "../ThemedView";
 import { Button } from "react-native-paper";
+import { Entypo } from "@expo/vector-icons";
 
-const Message = ({ message }: { message: string }) => <ThemedText type="subtitle" >{message}</ThemedText>;
+const Message = ({ message }: { message: string }) => (
+  <ThemedText style={styles.message} type="title">
+    {message}
+  </ThemedText>
+);
 
-export default function Game( {onEndGame} : {onEndGame: (didWin: boolean) => void}) {
+export default function Game({ onEndGame }: { onEndGame: (didWin: boolean) => void }) {
   
-  const [currentNumber, setCurrentNumber] = useState(95);
+  const [currentNumber, setCurrentNumber] = useState(0);
   const [isPlayerTurn, setIsPlayerTurn] = useState(true);
   const [isBotTurn, setIsBotTurn] = useState(false);
   const [buttonPressed, setButtonPressed] = useState(false);
@@ -19,20 +24,18 @@ export default function Game( {onEndGame} : {onEndGame: (didWin: boolean) => voi
   };
 
   const updateDisplayMessage = (message: string, nextNumber: number) => {
-
     setCurrentNumber(nextNumber + 1);
     setDisplayMessage(message);
 
     setButtonPressed(true);
     setIsPlayerTurn(false);
-    
+
     setTimeout(() => {
       setIsBotTurn(true);
       setButtonPressed(false);
       setDisplayMessage(null);
       handleBotTurn(currentNumber + 1);
     }, 700);
-
   };
 
   const handlePlayerCount = () => {
@@ -55,7 +58,6 @@ export default function Game( {onEndGame} : {onEndGame: (didWin: boolean) => voi
     if (newNumber >= 100) return onEndGame(true);
 
     if (isClap(newNumber + 1)) setDisplayMessage("Bộp!");
-
     else setDisplayMessage((newNumber + 1).toString());
 
     setTimeout(() => {
@@ -64,7 +66,6 @@ export default function Game( {onEndGame} : {onEndGame: (didWin: boolean) => voi
       setCurrentNumber(newNumber + 1);
       setIsPlayerTurn(true);
     }, 700); // Show message for 1 second
-
   };
 
   return (
@@ -73,9 +74,60 @@ export default function Game( {onEndGame} : {onEndGame: (didWin: boolean) => voi
         <ThemedText type="title" style={styles.bot}>
           Máy:
         </ThemedText>
-        {isBotTurn && displayMessage && <Message message={displayMessage} />}
+        {isBotTurn && displayMessage && (
+          <ThemedView style={styles.botCountContainer}>
+            <ThemedView style={styles.blockContainer}>
+              <Entypo
+                style={[styles.block, styles.botBlock1]}
+                name="progress-empty"
+                size={50}
+                color="black"
+              />
+              <Entypo
+                style={[styles.block, styles.botBlock2]}
+                name="progress-empty"
+                size={50}
+                color="black"
+              />
+              <Entypo
+                style={[styles.block, styles.botBlock3]}
+                name="progress-empty"
+                size={50}
+                color="black"
+              />
+            </ThemedView>
+            <Message message={displayMessage} />
+          </ThemedView>
+        )}
       </ThemedView>
       <ThemedView style={styles.playerContainer}>
+        <ThemedView style={styles.playerCountContainer}>
+          {buttonPressed && displayMessage && (
+            <ThemedView>
+              <Message message={displayMessage} />
+              <ThemedView style={styles.blockContainer}>
+                <Entypo
+                  style={[styles.block, styles.playerBlock1]}
+                  name="progress-empty"
+                  size={50}
+                  color="black"
+                />
+                <Entypo
+                  style={[styles.block, styles.playerBlock2]}
+                  name="progress-empty"
+                  size={50}
+                  color="black"
+                />
+                <Entypo
+                  style={[styles.block, styles.playerBlock3]}
+                  name="progress-empty"
+                  size={50}
+                  color="black"
+                />
+              </ThemedView>
+            </ThemedView>
+          )}
+        </ThemedView>
         {isPlayerTurn && (
           <ThemedView style={styles.buttonContainer}>
             <Button mode="outlined" onPress={handlePlayerCount}>
@@ -86,11 +138,6 @@ export default function Game( {onEndGame} : {onEndGame: (didWin: boolean) => voi
             </Button>
           </ThemedView>
         )}
-        <ThemedView style={styles.playerCountContainer}>
-          {buttonPressed && displayMessage && (
-            <Message message={displayMessage} />
-          )}
-        </ThemedView>
         <ThemedText type="title" style={styles.player}>
           Người chơi:
         </ThemedText>
@@ -120,5 +167,42 @@ const styles = StyleSheet.create({
   },
   playerCountContainer: {
     justifyContent: "flex-end",
+    paddingBottom: 60,
+  },
+  botCountContainer: {
+    paddingTop: 40,
+  },
+  message: {
+    textAlign: "center",
+  },
+  blockContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  block: {
+    padding: 12,
+  },
+  playerBlock1: {
+    transform: [{ rotate: "45deg" }],
+    marginBottom: -50,
+  },
+  playerBlock2: {
+    transform: [{ rotate: "90deg" }],
+  },
+  playerBlock3: {
+    transform: [{ rotate: "135deg" }],
+    marginBottom: -50,
+  },
+  botBlock1: {
+    transform: [{ rotate: "135deg" }],
+    marginTop: -50,
+  },
+  botBlock2: {
+    transform: [{ rotate: "90deg" }],
+  },
+  botBlock3: {
+    transform: [{ rotate: "45deg" }],
+    marginTop: -50,
   },
 });
